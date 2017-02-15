1d12da2c635cbf8860c804cd0ff9018e518ab24e
k2works <kakimomokuri@gmail.com>
2017-02-15 20:38:42 +0900

    アプリケーション開発 Step06

--- src/lib/frame.js
+++ src/lib/frame.js
@@ -1,14 +1,12 @@
 /**
  * Created by k2works on 2017/02/15.
  */
-// @flow
-import Throw from './throw'
 
 class Frame {
     getScore() {
         return 0
     }
-    add(t: Throw){
+    add(pins) {
     }
 }
 
