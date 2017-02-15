615f39e8859a0010614852e593c8dd29c57f02f7
k2works <kakimomokuri@gmail.com>
2017-02-15 21:00:52 +0900

    アプリケーション開発 Step09

---
+++ src/lib/game.js
@@ -0,0 +1,13 @@
+/**
+ * Created by k2works on 2017/02/15.
+ */
+class Game {
+    score() {
+        return 0;
+    }
+
+    add(pins) {
+    }
+}
+
+export default Game;
--- test/game_test.js
+++ test/game_test.js
@@ -2,6 +2,7 @@
  * Created by k2works on 2017/02/15.
  */
 import assert from 'power-assert';
+import Game from '../src/lib/game';
 
 describe('Game', () => {
     describe('.score', () => {
