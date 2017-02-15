db0245561345567f7d8382316abc1e649bfa1593
k2works <kakimomokuri@gmail.com>
2017-02-15 20:28:58 +0900

    アプリケーション開発 Step02

---
+++ test/frame_test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by k2works on 2017/02/15.
+ */
+import assert from 'power-assert';
+
+describe('Frame', () => {
+    it('does something useful', () => {
+        assert(true === true);
+    });
+});
