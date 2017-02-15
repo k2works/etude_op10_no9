541e85dc116252dabb086deb36d9acea9f10981b
k2works <kakimomokuri@gmail.com>
2017-02-15 20:32:25 +0900

    アプリケーション開発 Step03

---
+++ src/lib/frame.js
@@ -0,0 +1,10 @@
+/**
+ * Created by k2works on 2017/02/15.
+ */
+class Frame {
+    getScore() {
+        return 0
+    }
+}
+
+export default Frame;
--- test/frame_test.js
+++ test/frame_test.js
@@ -2,9 +2,15 @@
  * Created by k2works on 2017/02/15.
  */
 import assert from 'power-assert';
+import Frame from '../src/lib/frame'
 
 describe('Frame', () => {
-    it('does something useful', () => {
-        assert(true === true);
+    describe('.getScore', () => {
+        context('when no throw', function() {
+            it('is scored', () => {
+                var f = new Frame();
+                assert(f.getScore() === 0);
+            });
+        });
     });
 });
