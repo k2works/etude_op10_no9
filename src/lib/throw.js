0a9db64fb38959a515e8e017801fb5b28ad1de4a
k2works <kakimomokuri@gmail.com>
2017-02-15 19:29:52 +0900

    アプリケーション開発 Step01

---
+++ src/lib/throw.js
@@ -0,0 +1,7 @@
+/**
+ * Created by k2works on 2017/02/15.
+ */
+class Throw {
+}
+
+export default Throw;
---
+++ test/throw_test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by k2works on 2017/02/15.
+ */
+
+import assert from 'power-assert';
+import Throw from '../src/lib/throw';
+
+describe('Throw', () => {
+    it('does something useful', () => {
+        var obj = new Throw;
+        assert(obj === obj);
+    });
+});
