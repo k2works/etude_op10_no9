9fb25fb8cdf6c24116f09edbe7deecf1166bfdd3
k2works <kakimomokuri@gmail.com>
2017-02-15 21:02:13 +0900

    アプリケーション開発 Step10

--- src/lib/game.js
+++ src/lib/game.js
@@ -2,11 +2,16 @@
  * Created by k2works on 2017/02/15.
  */
 class Game {
+    constructor() {
+        this.itsScore = 0;
+    }
+
     score() {
-        return 0;
+        return this.itsScore;
     }
 
     add(pins) {
+        this.itsScore += pins;
     }
 }
 
