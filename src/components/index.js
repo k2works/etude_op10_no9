0ade05a26d9c8b5af635bf0df1c3ac86041cfbf9
k2works <kakimomokuri@gmail.com>
2017-02-15 17:23:45 +0900

    Initial Setup

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
-import './index.css';
 import _ from 'lodash'
 require("./index.scss");
 
-let contents = document.getElementById('contents')
+let contents = document.getElementById('contents');
 
 // Render Hello ,webpack
 function component () {
@@ -17,36 +16,3 @@ function component () {
 }
 
 contents.appendChild(component());
-
-// ES2015 arrow function
-[1,2,3].map(n => console.log(n + 1));
-
-// ES2015 Class
-import Human from '../lib/human';
-var obj = new Human('BABEL');
-var name = obj.hello();
-contents.appendChild(render(name));
-
-// Render ES2015 Class Object name
-function render (name) {
-  var element = document.createElement('div');
-  element.setAttribute('class', 'Contents__name');
-
-  /* lodash is required for the next line to work */
-  element.innerHTML = name;
-
-  return element;
-}
-
-// Rnder Images
-import jpg from './images/image.jpg'
-import png from './images/image.png'
-
-var box = document.getElementById('box');
-var imgJpg = document.createElement('img');
-imgJpg.src = jpg;
-box.appendChild(imgJpg);
-
-var imgPng = document.createElement('img');
-imgPng.src = png;
-box.appendChild(imgPng);
