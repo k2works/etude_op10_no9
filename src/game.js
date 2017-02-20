88ae49a9f167dc797c5e64048aeeaf5b82abda06
k2works <kakimomokuri@gmail.com>
2017-02-20 16:29:00 +0900

    開発 Step20
    ・Game.scoreForFrameテストメソッドを通るようにする

--- src/game.js
+++ src/game.js
@@ -22,7 +22,13 @@ class Game {
              currentFrame++) {
             let firstThrow = this.itsThrows[ball++];
             let secondThrow = this.itsThrows[ball++];
-            score += firstThrow + secondThrow;
+            let frameScore = firstThrow + secondThrow;
+            // スペアの得点計算には次のフレームの第１投が必要
+            if (frameScore === 10) {
+                score += frameScore + this.itsThrows[ball++];
+            } else {
+                score += frameScore;
+            }
         }
         return score;
     }
