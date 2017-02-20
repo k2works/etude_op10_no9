e5d313f250b8a97df18c6056c09695d85a773218
k2works <kakimomokuri@gmail.com>
2017-02-20 10:05:26 +0900

    開発 Step07
    ・Frame.getScoreメソッドの追加テストを通るようにする

--- src/frame.js
+++ src/frame.js
@@ -1,8 +1,14 @@
 class Frame {
-    getScore() {
-        return 0
+    constructor() {
+        this.itsScore = 0;
     }
+
+    getScore() {
+        return this.itsScore;
+    }
+
     add(pins) {
+        this.itsScore += pins;
     }
 }
 export default Frame;
