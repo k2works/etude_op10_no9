6e0f24d0d9c8d13aa3bb51cf6b597e2f8e26ed1c
k2works <kakimomokuri@gmail.com>
2017-02-20 15:21:29 +0900

    開発 Step14
    ・Game.scoreForFrameテストメソッドを通るようにする

--- src/game.js
+++ src/game.js
@@ -1,6 +1,8 @@
 class Game {
     constructor() {
         this.itsScore = 0;
+        this.itsThrows = Array.apply(null,Array(21)).map(function() { return 0 });
+        this.itsCurrentThrow = 0;
     }
 
     score() {
@@ -8,11 +10,18 @@ class Game {
     }
 
     add(pins) {
+        this.itsThrows[this.itsCurrentThrow++]=pins;
         this.itsScore += pins;
     }
 
     scoreForFrame(frame) {
-        return 0;
+        let score = 0;
+        for (let ball=0;
+             frame > 0 && (ball<this.itsCurrentThrow);
+             ball+2,frame--) {
+            score += this.itsThrows[ball] + this.itsThrows[ball+1];
+        }
+        return score;
     }
 }
 export default Game;
