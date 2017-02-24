06cab4c8532445358b1daad8dd9675d8eef62e2c
k2works <kakimomokuri@gmail.com>
2017-02-24 10:41:41 +0900

    開発 Step62
    ・Throwクラス削除

--- src/throw.js
+++
@@ -1,3 +0,0 @@
-class Throw {
-}
-export default Throw;
--- test/throw_test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'power-assert';
-import Throw from '../src/throw';
-
-describe('Throw', () => {
-    it('does something useful', () => {
-        var itsThrow = new Throw;
-        assert(itsThrow == itsThrow);
-    })
-})
