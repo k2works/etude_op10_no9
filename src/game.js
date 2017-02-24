357eae6f7187939438f18126a5156c06307dc603
k2works <kakimomokuri@gmail.com>
2017-02-24 09:49:19 +0900

    開発 Step53
    ・Scorerクラス追加
    ・Game.scoreForFrameのScorerクラスへの委譲

--- src/game.js
+++ src/game.js
@@ -1,10 +1,11 @@
+import Scorer from './scorer';
+
 class Game {
     constructor() {
         this.itsScore = 0;
-        this.itsThrows = Array.apply(null,Array(21)).map(function() { return 0 });
-        this.itsCurrentThrow = 0;
         this.itsFirstThrow = true;
         this.itsCurrentFrame = 1;
+        this.scorer = new Scorer;
     }
 
     score() {
@@ -12,53 +13,17 @@ class Game {
     }
 
     add(pins) {
-        this.itsThrows[this.itsCurrentThrow++]=pins;
+        this.scorer.addThrow(pins);
         this.itsScore += pins;
         this._adjustCurrentFrame(pins);
-    }
-
-    scoreForFrame(theFrame) {
-        this.ball=0;
-        let score = 0;
-        for (let currentFrame=0;
-             currentFrame < theFrame;
-             currentFrame++) {
-            if (this._strike()) {
-                score += 10 + this._nextTwoBallForSpare();
-                this.ball++;
-            } else if (this._spare()) {
-                score += 10 + this._nextBallForSpare();
-                this.ball+=2;
-            } else {
-                score += this._twoBallsInFrame();
-                this.ball+=2;
-            }
-        }
-        return score;
     }
 
     getCurrentFrame() {
         return this.itsCurrentFrame;
     }
 
-    _strike() {
-        return this.itsThrows[this.ball] === 10;
-    }
-
-    _nextTwoBallForSpare() {
-        return this.itsThrows[this.ball+1] + this.itsThrows[this.ball+2];
-    }
-
-    _spare() {
-        return (this.itsThrows[this.ball] + this.itsThrows[this.ball+1] === 10);
-    }
-
-    _nextBallForSpare() {
-        return this.itsThrows[this.ball+2];
-    }
-
-    _twoBallsInFrame() {
-        return this.itsThrows[this.ball] + this.itsThrows[this.ball+1];
+    scoreForFrame(theFrame) {
+        return this.scorer.scoreForFrame(theFrame);
     }
 
     _adjustCurrentFrame(pins) {
