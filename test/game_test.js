d3098074ed9d6aae5c56c4c54d247d8186e40ed8
k2works <kakimomokuri@gmail.com>
2017-03-03 19:40:53 +0900

    開発 デモデプロイ

--- test/game_test.js
+++ test/game_test.js
@@ -3,55 +3,55 @@ import Game from '../src/game';
 
 describe('Game', () => {
     let g;
-    beforeEach(function () {
+    beforeEach(function() {
         g = new Game();
     });
 
-    let _twoThrows = function () {
+    let _twoThrows = function() {
         g.add(5);
         g.add(4);
     };
-    let _fourThrows = function () {
+    let _fourThrows = function() {
         g.add(5);
         g.add(4);
         g.add(7);
         g.add(2);
     };
-    let _spareAndOneThrow = function () {
+    let _spareAndOneThrow = function() {
         g.add(3);
         g.add(7);
         g.add(3);
     };
-    let _spareAndTwoThrows = function () {
+    let _spareAndTwoThrows = function() {
         g.add(3);
         g.add(7);
         g.add(3);
         g.add(2);
     };
-    let _nineThrowsAndSpare = function () {
+    let _nineThrowsAndSpare = function() {
         for (let i = 0; i < 9; i++)
             g.add(10);
         g.add(9);
         g.add(1);
         g.add(1);
     };
-    let _strikeAndOneThrow = function () {
+    let _strikeAndOneThrow = function() {
         g.add(10);
         g.add(3);
         g.add(6);
     };
-    let _perfectGame = function () {
+    let _perfectGame = function() {
         for (let i = 0; i < 12; i++) {
             g.add(10);
         }
     };
-    let _eighteenThrowsGutter = function () {
+    let _eighteenThrowsGutter = function() {
         for (let i = 0; i < 9; i++) {
             g.add(0);
             g.add(0);
         }
     };
-    let _oneSampleGame = function () {
+    let _oneSampleGame = function() {
         g.add(1);
         g.add(4);
         g.add(4);
@@ -72,20 +72,20 @@ describe('Game', () => {
         g.add(8);
         g.add(6);
     };
-    let _heartBrakeGame = function () {
+    let _heartBrakeGame = function() {
         for (let i = 0; i < 11; i++)
             g.add(10);
         g.add(9);
     };
 
     describe('.score', () => {
-        context('when two throws', function () {
+        context('when two throws', function() {
             it('is scored', () => {
                 _twoThrows();
                 assert(g.score() === 9);
             })
         });
-        context('when spare', function () {
+        context('when spare', function() {
             it('is scored', () => {
                 _spareAndOneThrow();
                 assert(g.score() === 13);
@@ -99,34 +99,34 @@ describe('Game', () => {
                 assert(g.score() === 270);
             })
         });
-        context('when strike', function () {
+        context('when strike', function() {
             it('is scored', () => {
                 _strikeAndOneThrow();
                 assert(g.score() === 28);
             })
         });
-        context('when perfect game', function () {
+        context('when perfect game', function() {
             it('is scored', () => {
                 _perfectGame();
                 assert(g.score() === 300);
             })
         });
-        context('when 10th frame', function () {
+        context('when 10th frame', function() {
             it('is scored', () => {
                 _eighteenThrowsGutter();
                 g.add(2);
-                g.add(8);  // 10番目のフレームのスペア
+                g.add(8); // 10番目のフレームのスペア
                 g.add(10); // 配列の最後にストライク
                 assert(g.score() === 20);
             })
         });
-        context('when playing one game', function () {
+        context('when playing one game', function() {
             it('is scored', () => {
                 _oneSampleGame();
                 assert(g.score() === 133);
             })
         });
-        context('when heart break game', function () {
+        context('when heart break game', function() {
             it('is scored', () => {
                 _heartBrakeGame();
                 assert(g.score() === 299);
@@ -135,14 +135,14 @@ describe('Game', () => {
     });
 
     describe('.scoreForFrame', () => {
-        context('when four throws', function () {
+        context('when four throws', function() {
             it('is scored by frame', () => {
                 _fourThrows();
                 assert(g.scoreForFrame(1) === 9);
                 assert(g.scoreForFrame(2) === 18);
             })
         });
-        context('when spare', function () {
+        context('when spare', function() {
             it('is scored by frame', () => {
                 _spareAndOneThrow();
                 assert(g.scoreForFrame(1) === 13);
@@ -152,7 +152,7 @@ describe('Game', () => {
                 assert(g.scoreForFrame(2) === 18);
             })
         });
-        context('when strike', function () {
+        context('when strike', function() {
             it('is scored by frame', () => {
                 _strikeAndOneThrow();
                 assert(g.scoreForFrame(1) === 19);
